c8c1ae593101a1ede82b54249cb098baea6c64d6
Zabellfour <zabell2814@gmail.com>
2017-08-10 00:15:26 +0300

    added part of header

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,15 @@
-// You can write a call and import your functions in this file.
-//
-// This file will be compiled into app.js and will not be minified.
-// Feel free with using ES6 here.
+(($) => {
+    'use strict';
 
-import DE from './modules/dots';
+    // When DOM is ready
+    $(() => {
+        $('.drop-opener').on("click", function() {
+            $(this).toggleClass('opened');
+            $('.drop').toggleClass('active');
+        });
+    });
 
-( ($) => {
-  'use strict';
 
-  // When DOM is ready
-  $(() => {
-    DE.dotsEffect();
-  });
+
 
 })(jQuery);
