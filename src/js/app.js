844ae57ca65bc74a0a241e6403b19d19b897c47a
Zabellfour <zabell2814@gmail.com>
2017-08-11 01:58:17 +0300

    added banner and header + mobile

--- src/js/app.js
+++ src/js/app.js
@@ -7,6 +7,26 @@
             $(this).toggleClass('opened');
             $('.drop').toggleClass('active');
         });
+        $('.menu-opener').on("click", function() {
+            $(this).toggleClass('opened');
+            $('body').toggleClass('menu-open');
+        });
+        $('.menu-close').on("click", function() {
+            $('.menu-opener').removeClass('opened');
+            $('body').removeClass('menu-open');
+        });
+        var player = videojs('bg-player', options, function onPlayerReady() {
+            videojs.log('Your player is ready!');
+
+            // In this context, `this` is the player that was created by Video.js.
+            this.play();
+
+            // How about an event listener?
+            this.on('ended', function() {
+                videojs.log('Awww...over so soon?!');
+            });
+        });
+
     });
 
 
