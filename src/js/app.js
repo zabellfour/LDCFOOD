d6d8bec6127853b41d468e89e14aedc40d7ec25e
Alit4inov <alit4inov@gmail.com>
2017-08-15 23:54:52 +0300

    start resp filter products grid

--- src/js/app.js
+++ src/js/app.js
@@ -25,6 +25,37 @@
 		        $('.callback').removeClass('active');
 		    });
 
+		    $('.f-attribute-list > li').on("click", function() {
+	            $(this).toggleClass('selected');
+	            return false;
+	        });
+
+	        $('.m-filter-opener').on("click", function() {
+	            $(this).toggleClass('opened');
+	            $('body').toggleClass('m-filter-open');
+	        });
+	        $('.m-filter-close').on("click", function() {
+	            $('.m-filter-opener').removeClass('opened');
+	            $('body').removeClass('m-filter-open');
+	        });
+
+			(function($) {
+			    var $window = $(window),
+			        $nst = $('.nst-component');
+
+			    function resize() {
+			        if ($window.width() < 768) {
+			            return $nst.addClass('nst-is-collapsed');
+			        }
+
+			        $nst.removeClass('nst-is-collapsed');
+			    }
+
+			    $window
+			        .resize(resize)
+			        .trigger('resize');
+			})(jQuery);
+
 	        var videos = $(".video-banner video"),
 	            fraction = 0.35;
 
